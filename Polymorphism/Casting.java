585cf6d68e5a803cbfbffe96dbdef3cf2b72b09c
Oinacaru Adrian <127617426+Adrian20040101@users.noreply.github.com>
2023-09-03 14:04:24 +0300

    Update Casting.java

--- Polymorphism/Casting.java
+++ Polymorphism/Casting.java
@@ -19,5 +19,17 @@ public class Casting {
         //we can also create objects using var -> this is called Local Variable Type Inference (LVTI)
         var plane = new ScienceFiction("Plane");
         plane.watchMovie();
+
+        //we can also test the type of the object created at runtime. Helps to better visualise which method and from which class the method should be executed
+        Object unknown = Movie.getMovie("A", "John Wick");
+        if (unknown.getClass().getSimpleName().equals("Adventure")){ //could have also used '==' to compare the two
+            Adventure adventure = (Adventure) unknown;
+            adventure.watchMovie();
+        } else if (unknown instanceof Adventure){  //the instanceof operator lets us test the type of an object or an instance
+            ((Adventure) unknown).watchMovie();
+        } else if (unknown instanceof ScienceFiction scifi){  //if Java can identify that the object matches the type, it can extract data without having to cast. The object can be assigned to a binding variable and this variable is already typed as ScienceFiction variable
+            scifi.watchMovie();
+        }
     }
 }
+
